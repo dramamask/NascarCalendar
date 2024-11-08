32d69fdb1876365d9f351060ef93509eae886768
Mark Daams <mark.daams@vasion.com>
2024-11-07 18:32:26 -0700

    Gitignore stuff

--- obj/Debug/net8.0/NascarCalendar.AssemblyInfo.cs
+++ obj/Debug/net8.0/NascarCalendar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NascarCalendar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+031167238ac255a87a273e897ff891c5a5599bea")]
 [assembly: System.Reflection.AssemblyProductAttribute("NascarCalendar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NascarCalendar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
