e1242e687f3b77778e616a07740db19bdda7e41b
Mark Daams <mark.daams@vasion.com>
2024-11-07 19:08:14 -0700

    Refactor http get

--- Services/ServerNascarService.cs
+++ Services/ServerNascarService.cs
@@ -6,7 +6,15 @@ public class ServerNascarService(HttpClient http) : INascarService
 {
     public async Task<string> GetCalendar()
     {
-        var uri = "https://cf.nascar.com/cacher/2023/race_list_basic.json";
-        return await http.GetStringAsync(uri);
+        HttpResponseMessage res = await http.GetAsync("https://cf.nascar.com/cacher/2023/race_list_basic.json");
+
+        if (!res.IsSuccessStatusCode)
+        {
+            string msg = await res.Content.ReadAsStringAsync();
+            // TODO: Log the error somewhere
+            throw new Exception(msg);
+        }
+
+        return await res.Content.ReadAsStringAsync();
     }
 }
--- obj/Debug/net8.0/NascarCalendar.AssemblyInfo.cs
+++ obj/Debug/net8.0/NascarCalendar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NascarCalendar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d69fdb1876365d9f351060ef93509eae886768")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8255f8b61a866f0d91cd79a3915ba7a44aec6384")]
 [assembly: System.Reflection.AssemblyProductAttribute("NascarCalendar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NascarCalendar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
