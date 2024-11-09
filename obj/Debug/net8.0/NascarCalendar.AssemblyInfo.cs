ca6f0b729f60fe3695568ed7e2923fce293e2f3b
Mark Daams <mark.daams@vasion.com>
2024-11-08 19:26:30 -0700

    intermediate commit

--- obj/Debug/net8.0/NascarCalendar.AssemblyInfo.cs
+++ obj/Debug/net8.0/NascarCalendar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NascarCalendar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c893253a0f4d26b9448efb15dbf3c13bf5071f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2119f89ee082569cca0bb69b68910581557f467")]
 [assembly: System.Reflection.AssemblyProductAttribute("NascarCalendar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NascarCalendar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
